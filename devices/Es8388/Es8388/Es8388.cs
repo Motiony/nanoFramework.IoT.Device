2a61c24724a460e37db43e33b3635d7edaf7fea0
matihuf <matihuf@hotmail.com>
2023-03-06 10:42:33 +0100

    Es8388 device init

---
+++ devices/Es8388/Es8388/Es8388.cs
@@ -0,0 +1,9 @@
+﻿namespace Es8388
+{
+    public class Es8388
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
